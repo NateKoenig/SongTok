d755a99d1987c5c40fc811b939b3b46b3c95a075
Nate Koenig <55851808+NateKoenig@users.noreply.github.com>
2021-05-16 15:26:35 -0400

    Add files via upload

---
+++ webhost2.py
@@ -0,0 +1,64 @@
+from flask import Flask, redirect, render_template, url_for, request
+import validators
+import requests
+
+#import re
+#from bs4 import BeautifulSoup
+#import urllib as DFU
+#import os
+#from termcolor import colored
+
+from TikTokApi import TikTokApi
+import string
+import random
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+	if request.method == "POST":
+		url_input = request.form["link"]
+		valid = validators.url('{}'.format(url_input))
+		if valid == True:
+			#TODO:
+			# - Fix the url, it's a tik tok link and need it to be an mp3
+			# - Check if valid tik tok link, not just valid url (we can just given an error too, not a big deal)
+			# - Output song on a different page? Use redirect if so. If not, how to output/display?
+			# - If invalid url, display a message saying so
+
+			#From https://docs.audd.io
+			data = {
+	    		'api_token': '4d50671c81da7437891a11846780852b',
+	    		'url': '{}'.format(url_input),
+			}
+			result = requests.post('https://api.audd.io/recognizeWithOffset/', data=data)
+			#console.log(result)
+			return result.text
+		else:
+			return render_template("index.html")
+	else:
+
+		return render_template("index.html")
+
+
+if __name__ == "__main__":
+	app.run()
+
+
+#Code to download tik toks
+from TikTokApi import TikTokApi
+import string
+import random
+did=''.join(random.choice(string.digits) for num in range(19))
+verifyFp="verify_kon4ht8t_cXlUzSMR_D9IQ_4viA_BfCh_Z6QwNIYLNtIf"
+#api = TikTokApi.get_instance(custom_verifyFp=verifyFp, custom_did=did)
+api = TikTokApi.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
+print(api.trending())
+
+from pathlib import Path
+Path("downloads").mkdir(exist_ok=True) # creates folder
+for i in range(len(tiktoks)):
+    data = api.get_Video_By_TikTok(tiktoks[i])# bytes of the video
+    with open("downloads/{}.mp4".format(str(i)), 'wb') as output:
+        output.write(data) # saves data to the mp4 file
+
